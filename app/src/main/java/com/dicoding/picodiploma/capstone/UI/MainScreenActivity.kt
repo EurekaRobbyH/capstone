a6e772119e2c106cd9a3fb37955000e4b4aade61
EurekaRobbyH <eureka.robby09@gmail.com>
2022-06-06 20:02:21 +0700

    home xml

---
+++ app/src/main/java/com/dicoding/picodiploma/capstone/UI/MainScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.picodiploma.capstone.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+    }
+}
