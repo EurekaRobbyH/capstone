a3e6fb5d57473bfdb0e12278213f6347df588a5d
EurekaRobbyH <eureka.robby09@gmail.com>
2022-06-08 17:54:32 +0700

    home xml

--- app/src/main/java/com/dicoding/picodiploma/capstone/UI/MainScreenActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/capstone/UI/MainScreenActivity.kt
@@ -2,10 +2,13 @@ package com.dicoding.picodiploma.capstone.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dicoding.picodiploma.capstone.R
 
 class MainScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
+
+        supportActionBar?.hide()
     }
 }
